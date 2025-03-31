e59ea570a763672dedfb434067c1d89ac25549a9
Maria Antonova <maria.antonova@metropolia.fi>
2025-03-31 14:28:59 +0300

    Updated task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    //Loop to print even numbers from 1 to 50
-    for (int i=2; i<=50; i+=2){
-        cout<< i<< " ";
+#define UPPER_LIMIT 50  // MACRO for the upper limit
+
+int main() {
+    // Loop to print even numbers from 1 to 50
+    for (unsigned char i = 2; i <= UPPER_LIMIT; i += 2) {
+        cout << static_cast<int>(i) << " ";  // Cast to int for correct display
     }
- 
+
+    return 0;
 }
